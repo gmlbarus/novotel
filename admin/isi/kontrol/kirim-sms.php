cd47ffd5d3599bcc0ddd437901a736051cfe457d
gmlbarus <gmlbarus@gmail.com>
2017-02-16 09:05:28 +0700

    Modify kirim-sms.php change wrong use of mysql_real_escape_string, mysql_real_escape_string has to be implemented after a mysql connection is established

--- admin/isi/kontrol/kirim-sms.php
+++ admin/isi/kontrol/kirim-sms.php
@@ -1,40 +1,51 @@
 <?php
+var_dump($_POST);
 //include '../../../login/hub.php';
-$sms = mysql_real_escape_string(strip_tags($_POST['sms']));
-$no = mysql_real_escape_string($_POST['no']);
-$sub= mysql_real_escape_string($_POST['sub']);
+$sms = strip_tags($_POST['sms']);
+$no = $_POST['no'];
+$sub= $_POST['sub'];
 	if(empty($sms) or empty($no))
-	{echo "<script>alert('Data tidak lengkap!');
+	{
+		echo "<script>alert('Data tidak lengkap!');
 			document.location='../../?page=sms'
 			</script>";
 	}
 	else
 	{	echo 'nak masuk input';
 		if($sub=='input'){
-			$sand='';
+			$sand='root';
 			$pengg='root';
 			$basis='sms-limas';
 			$lewat=mySQL_escape_string($sand);
 			$lewat2=mySQL_escape_string($pengg);
 			$lewat3=mySQL_escape_string($basis);
-	
-			if($db=mysql_connect("localhost", $lewat2, $lewat))
+
+			if($db=mysql_connect("localhost", $pengg, $sand))
 			{
-				mysql_select_db($lewat3, $db) or header('location: ../../../login/error.php');
+				mysql_select_db($basis, $db) or header('location: ../../../login/error.php');
 				echo "sukses";
 			}
 			else
 			{echo "gagal";
 				header('location: ../../../login/error.php');
 			}
-			
-			$simpan=mysql_query('insert into outbox(DestinationNumber, TextDecoded,
+			$query = sprintf("insert into outbox(DestinationNumber, TextDecoded,
 							CreatorID, Class)
-							values("'.$no.'","'.$sms.'","Limas", 0)');
+							values('%s','%s','Limas', 0)",
+            mysql_real_escape_string($no),
+            mysql_real_escape_string($sms));
+			$simpan = mysql_query($query);
+
+			// $simpan=mysql_query('insert into outbox(DestinationNumber, TextDecoded,
+			// 				CreatorID, Class)
+			// 				values("'.$no.'","'.$sms.'","Limas", 0)');
 			if($simpan){
-				echo "<script>
-						document.location='../../?page=sms';
-						</script>";
+				// echo "<script>
+				// 		document.location='../../?page=sms';
+				// 		</script>";
+				echo "<script>alert('SMS berhasil terkirim');
+					document.location='../../?page=sms'
+					</script>";
 			}
 			else{
 			echo mysql_error();
